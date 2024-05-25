11c752f19408dfbc9e3041ec0ab4e79449a3fe6f
2006dll <lvedileva73@gmail.com>
2024-05-25 17:34:26 -0300

    nuevo

---
+++ JS/src/json.js
@@ -0,0 +1,25 @@
+class productosBelleza = {
+   constructor (nombre, marca, modoDeUso, precio){
+        this.nombre=nombre;
+        this.marca=marca;
+        this.modoDeUso=modoDeUso;
+        this.precio=precio;
+   }
+   
+};
+
+// API - Application Programming Interface
+// Consumiendo una API
+// https://username.github.io/reponame/file.json
+
+fetch("https://2006dll.github.io/Codoacodo24264mio/datos.json")
+.then(response => response.json())
+.then(data => {
+    // Procesamiento de la info que llega de la API
+    console.log(data);
+    })
+.catch(error => console.log("Ocurrió un error! " + error));
+
+
+
+
