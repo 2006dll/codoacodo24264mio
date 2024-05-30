ddd52515d712ce32d14cc8d69eab48a98e5b547c
2006dll <lvedileva73@gmail.com>
2024-05-30 10:18:44 -0300

    lista de objetos

--- JS/src/json.js
+++ JS/src/json.js
@@ -14,16 +14,13 @@ let CremaCorporalBodyMilk = new productoDeBelleza("Crema Corporal.Body Milk","Ca
 
 let ContornodeOjosEyeCream = new productoDeBelleza("Contorno de Ojos.Eye Cream.","Carpe Diem","Con las manos higienizadas, aplicar sobre el dedo anular y dar suaves toquecitos hasta que absorba, sin frotar ni estirar debajo y a lo largo del ojo. No se recomienda aplicar en el párpado superior. Utilizar hasta 2 veces por día.",29000)
 
-console.log (AceiteFacialAntioxidante)
-console.log (CremaCorporalBodyMilk)
-console.log (ContornodeOjosEyeCream)
 
 
 // API - Application Programming Interface
 // Consumiendo una API
 // https://username.github.io/reponame/file.json
 
-fetch("https://2006dll.github.io/Codoacodo24264mio/datos.json")
+fetch("https://2006dll.github.io/codoacodo24264mio/datos.json")
 .then(response => response.json())
 .then(data => {
     // Procesamiento de la info que llega de la API
